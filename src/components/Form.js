84ed00c274e68c4443c92b1aed1c2d4fb273dfe1
Sahil <smehar@aceproject.space>
2021-05-28 15:00:08 -0500

    Todo List is done

---
+++ src/components/Form.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Form = ({setInputText ,inputText, todos ,setTodos ,setStatus }) => {
+//Here i can write js code
+const inputTextHandler = (e) =>{
+    console.log(e.target.value);
+    setInputText(e.target.value)
+}
+const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([...todos, {text : inputText ,
+        completed: false,
+        id: Math.random() * 1000
+    }]);
+    setInputText("");
+
+}
+const StatusHandler = (e) =>{
+    setStatus(e.target.value);
+}
+
+    return (
+        <div>
+            <form>
+      <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select onChange={StatusHandler} name="todos" className="filter-todo">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
+      </div>
+    </form>
+        </div>
+    )
+}
+
+export default Form
---
+++ src/components/TodoList.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Todo from './Todo'
+
+const TodoList = ({todos ,setTodos ,filterTodos }) => {
+    
+
+    return (
+        <div className="todo-container">
+        <ul className="todo-list">
+         
+           {filterTodos.map(todo => (
+               <Todo
+               todos={todos} 
+               setTodos={setTodos}
+               key={todo.id}
+               todo={todo}
+               text={todo.text}  />
+           ))}
+        
+        </ul>
+      </div>
+    )
+}
+
+export default TodoList
